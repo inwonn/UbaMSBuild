da72fc467f3d2a81f0f9966cb3d6c1c1fc68ae13
DESKTOP-6CFMAOV\asdjk <inwon888@gmail.com>
2024-01-14 22:35:18 +0900

    줄바꿈 문자 추가

--- Shared/Core/dllmain.cpp
+++ Shared/Core/dllmain.cpp
@@ -36,30 +36,30 @@ BOOL WINAPI Detoured_CreateProcessW(
     _Out_ LPPROCESS_INFORMATION lpProcessInformation
 )
 {
-    DEBUG_LOG(L"Detoured ---> %s", lpCommandLine);
+    DEBUG_LOG(L"Detoured ---> %s\n", lpCommandLine);
     if (boost::icontains(lpCommandLine, "msbuild.exe"))
     {
-        DEBUG_LOG(L"Should be detour. ---> %s", lpCommandLine);
+        DEBUG_LOG(L"Should be detour. ---> %s\n", lpCommandLine);
 
         return DetourCreateProcessWithDll(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation, "D:\\Git\\UBAVS\\Binaries\\x64\\Debug\\Core.Test\\Core.dll", True_CreateProcessW);
     }
     else if (boost::icontains(lpCommandLine, "tracker.exe"))
     {
-        DEBUG_LOG(L"Should be send to host. ---> %s", lpCommandLine);
+        DEBUG_LOG(L"Should be send to host. ---> %s\n", lpCommandLine);
 
         ubavs::BuildMessage message;
-        DEBUG_LOG(L"CommandLine : %s", lpCommandLine);
+        DEBUG_LOG(L"CommandLine : %s\n", lpCommandLine);
         std::string commandLine = boost::locale::conv::utf_to_utf<char>(lpCommandLine);
         message.set_commandline(commandLine);
 
-        DEBUG_LOG(L"CurrentDirectory : %s", lpCurrentDirectory);
+        DEBUG_LOG(L"CurrentDirectory : %s\n", lpCurrentDirectory);
         std::string currentDirectory = boost::locale::conv::utf_to_utf<char>(lpCurrentDirectory);
         message.set_currentdirectory(boost::locale::conv::utf_to_utf<char>(currentDirectory));
 
         const wchar_t* lpEnv = static_cast<const wchar_t*>(lpEnvironment);
         while (*lpEnv != L'\0')
         {
-            DEBUG_LOG(L"Env : %s", lpEnv);
+            DEBUG_LOG(L"Env : %s\n", lpEnv);
             std::string env = boost::locale::conv::utf_to_utf<char>(lpEnv);
             message.add_environment(env);
 
@@ -72,16 +72,16 @@ BOOL WINAPI Detoured_CreateProcessW(
             std::string serializedMessage = message.SerializeAsString();
             std::wstring serializedMessageW = boost::locale::conv::utf_to_utf<wchar_t>(serializedMessage);
 
-            DEBUG_LOG(L"Send ---> %s", serializedMessageW.c_str());
+            DEBUG_LOG(L"Send ---> %s\n", serializedMessageW.c_str());
 	        sharedMemory.Write(serializedMessageW.c_str());
         }
         catch (const std::exception& e)
         {
-			DEBUG_LOG(L"Exception : %s", e.what());
+			DEBUG_LOG(L"Exception : %s\n", e.what());
 		}
         catch (...)
         {
-            DEBUG_LOG(L"Exception : %s");
+            DEBUG_LOG(L"Exception : %s\n");
         }*/
     }
 
@@ -100,7 +100,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        DEBUG_LOG(L"DLL_PROCESS_ATTACH");
+        DEBUG_LOG(L"DLL_PROCESS_ATTACH\n");
         DetourRestoreAfterWith();
 		DetourTransactionBegin();
 		DetourUpdateThread(GetCurrentThread());
