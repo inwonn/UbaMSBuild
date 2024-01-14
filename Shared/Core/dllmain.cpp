5108d76e0ac3a9679f159ff10d5efb796997f87e
DESKTOP-6CFMAOV\asdjk <inwon888@gmail.com>
2024-01-14 13:28:03 +0900

    protobuf 메시지 추가 triplet 을 windows-static 으로 변경 project.filter 설정 및 폴더 정리

--- Shared/Core/dllmain.cpp
+++ Shared/Core/dllmain.cpp
@@ -1,8 +1,11 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
-#include "Core.h"
+#include "Core/Core.h"
 
 #include <detours/detours.h>
+#include <boost/algorithm/string.hpp>
+#include "Messages/BuildMessage.pb.h"
+#include <boost/locale/encoding_utf.hpp>
 
 BOOL (*True_CreateProcessW)(
     _In_opt_ LPCWSTR lpApplicationName,
@@ -31,6 +34,40 @@ BOOL WINAPI Detoured_CreateProcessW(
 )
 {
     DEBUG_LOG(L"Detoured ---> %s", lpCommandLine);
+    if (boost::icontains(lpCommandLine, "msbuild.exe"))
+    {
+        DEBUG_LOG(L"Should be detour. ---> %s", lpCommandLine);
+
+        return DetourCreateProcessWithDll(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation, "D:\\Git\\UBAVS\\Binaries\\x64\\Debug\\Core.Test\\Core.dll", True_CreateProcessW);
+    }
+    else if (boost::icontains(lpCommandLine, "tracker.exe"))
+    {
+        DEBUG_LOG(L"Should be send to host. ---> %s", lpCommandLine);
+
+        ubavs::BuildMessage message;
+        DEBUG_LOG(L"CommandLine : %s", lpCommandLine);
+        std::string commandLine = boost::locale::conv::utf_to_utf<char>(lpCommandLine);
+        message.set_commandline(commandLine);
+
+        DEBUG_LOG(L"CurrentDirectory : %s", lpCurrentDirectory);
+        std::string currentDirectory = boost::locale::conv::utf_to_utf<char>(lpCurrentDirectory);
+        message.set_currentdirectory(boost::locale::conv::utf_to_utf<char>(currentDirectory));
+
+        // get environment list
+        LPWCH env = GetEnvironmentStringsW();
+        LPWSTR lpszVariable;
+        LPWSTR lpszValue;
+
+        const wchar_t* lpEnv = static_cast<const wchar_t*>(lpEnvironment);
+        while (*lpEnv != L'\0')
+        {
+            DEBUG_LOG(L"Env : %s", lpEnv);
+            std::string env = boost::locale::conv::utf_to_utf<char>(lpEnv);
+            message.add_environment(env);
+
+            lpEnv += env.size() + 1;
+        }
+    }
 
     return True_CreateProcessW(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
 }
